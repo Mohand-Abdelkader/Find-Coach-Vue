52406a7421a3a4d38acda8147a9ea2056a269571
Mohand Abdelkader <mohand27m@gmail.com>
2025-03-16 06:25:40 +0200

    use vuex in coaches list

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,8 +5,24 @@
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>
-      list of coaches
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filterCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
+    <h2 v-else>There is No Coaches found</h2>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filterCoaches() {
+      return this.$store.getters["coaches/coaches"];
+    },
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
+</script>
