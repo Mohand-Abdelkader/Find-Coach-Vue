365ef99ea46250af0bd1c6f4492ff3e1179d8bc5
Mohand Abdelkader <mohand27m@gmail.com>
2025-03-17 09:30:20 +0200

    page not found

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,1 +1,9 @@
-<template>this not found page</template>
+<template>
+  <base-card>
+    <h2>Page Not found</h2>
+    <p>
+      this page could not be found - maybe check out all our
+      <router-link to="/coaches">coaches</router-link>
+    </p>
+  </base-card>
+</template>
