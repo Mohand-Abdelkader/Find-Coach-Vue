66692fea57173226192009ed13022d8257643d5a
Mohand Abdelkader <mohand27m@gmail.com>
2025-03-18 03:40:59 +0200

    auth added and signup

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,29 @@
+export default {
+  async signup(context, payload) {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${
+        import.meta.env.VITE_FIREBASE_API_KEY
+      }`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      const error = new Error(responseData.message || "Failed to signup");
+      throw error;
+    }
+    console.log(responseData);
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
